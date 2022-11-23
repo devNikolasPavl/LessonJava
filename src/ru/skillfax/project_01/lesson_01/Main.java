2b680e31e0188a1ec45830509ebf401c4cdc5bd8
Nikolas Pavlov <devnikolaspavl@gmail.com>
2022-11-23 17:09:06 +0300

    Lesson 1 & 2. Первые строки кода

---
+++ src/ru/skillfax/project_01/lesson_01/Main.java
@@ -0,0 +1,12 @@
+package ru.skillfax.project_01.lesson_01;
+
+public class Main
+{
+    //Так будут указаны комментарии в коде. Это ваши подсказки.
+    private static String text = "Hello world!";
+
+    public static void main(String[] args)
+    {
+        System.out.println(text);
+    }
+}
---
+++ src/ru/skillfax/project_01/lesson_02/Main.java
@@ -0,0 +1,17 @@
+package ru.skillfax.project_01.lesson_02;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //↓ Здесь можно изменить значение первого числа
+        int a = 5;
+        //↓ Здесь можно изменить значение второго числа
+        int b = 5;
+
+        int result = a + b;
+
+        //↓ Эта строка отвечает за вывод информации на экран
+        System.out.println(result);
+    }
+}
