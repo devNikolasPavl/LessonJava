78a7c6b7332105503750f3c4eeccdfdd4f0466b4
Nikolas Pavlov <devnikolaspavl@gmail.com>
2022-11-27 17:04:09 +0300

    Java_marathon, day 1

---
+++ src/ru/javamarathon/day1/Task5.java
@@ -0,0 +1,22 @@
+package ru.javamarathon.day1;
+
+/**
+ * @author Nikolas Pavlov
+ *
+ * Повторить задание 4, но используя цикл for
+ */
+
+public class Task5
+{
+    public static void main(String[] args)
+    {
+        int age = 42;
+
+        for (int i = age; i <= 100; i += 5)
+        {
+            System.out.println("Мой возраст " + i);
+
+        }
+
+    }
+}
