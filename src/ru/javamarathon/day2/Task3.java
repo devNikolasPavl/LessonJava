3d4473ccb1e88b89aab218fe6f34b72c8c6c5b12
Nikolas Pavlov <devnikolaspavl@gmail.com>
2022-11-27 18:28:07 +0300

    Java_marathon, day 2

---
+++ src/ru/javamarathon/day2/Task3.java
@@ -0,0 +1,32 @@
+package ru.javamarathon.day2;
+
+import java.util.Scanner;
+
+/**
+ * @author Nikolas Pavlov
+ *
+ * Реализовать аналогичную программу №2, используя цикл while
+ */
+
+public class Task3
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        int t = a + 1;
+        while (t < b)
+        {
+            if (t % 5 == 0 && t % 10 != 0)
+            {
+                System.out.println(t);
+            }
+            t++;
+
+        }
+
+    }
+}
