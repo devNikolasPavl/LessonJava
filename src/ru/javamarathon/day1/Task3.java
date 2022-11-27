881b2075c0bb08221f352de3cf4d1e80d4109bf9
Nikolas Pavlov <devnikolaspavl@gmail.com>
2022-11-27 16:53:15 +0300

    Java_marathon, day 1

---
+++ src/ru/javamarathon/day1/Task3.java
@@ -0,0 +1,20 @@
+package ru.javamarathon.day1;
+
+/**
+ * @author  Nikolas Pavlov
+ *
+ * Вывести на экран слово "JAVA", в столбик, чтобы оно повторилось 10 раз, используя for
+ */
+
+public class Task3
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println("JAVA");
+
+        }
+
+    }
+}
