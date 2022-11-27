3cf6ce888e0322e826d67573a687ed18dcb9c4f9
Nikolas Pavlov <devnikolaspavl@gmail.com>
2022-11-27 16:50:16 +0300

    Java_marathon, day 1

---
+++ src/ru/javamarathon/day1/Task2.java
@@ -0,0 +1,20 @@
+package ru.javamarathon.day1;
+
+/**
+ * @author Nikolas Pavlov
+ *
+ * Повторить задание 1, но используя цикл for
+ */
+
+public class Task2
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println("JAVA ");
+
+        }
+
+    }
+}
