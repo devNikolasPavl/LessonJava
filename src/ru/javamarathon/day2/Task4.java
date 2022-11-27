d9e20c4e6886b525bc4b5d2ee857acd5aac0097b
Nikolas Pavlov <devnikolaspavl@gmail.com>
2022-11-27 18:37:10 +0300

    Java_marathon, day 2

---
+++ src/ru/javamarathon/day2/Task4.java
@@ -0,0 +1,30 @@
+package ru.javamarathon.day2;
+
+import java.util.Scanner;
+
+/**
+ * @author Nikolas Pavlov
+ */
+
+public class Task4
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double x = scanner.nextDouble();
+        double y = 0;
+
+        if (x >= 5)
+        {
+            y = (Math.pow(x, 2) - 10) / (x + 7);
+        } else if (x > -3 && x < 5)
+        {
+            y = (x + 3) * (Math.pow(x, 2) - 2);
+        } else
+        {
+            y = 420;
+        }
+
+        System.out.println(y);
+    }
+}
