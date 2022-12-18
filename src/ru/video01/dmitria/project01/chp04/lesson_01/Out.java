3793cbbf9400f6b4ded801b0dee8e5167fcc24d5
Nikolas Pavlov <devnikolaspavl@gmail.com>
2022-12-18 14:00:26 +0300

    video01. project01. chp04. lesson_01

---
+++ src/ru/video01/dmitria/project01/chp04/lesson_01/Out.java
@@ -0,0 +1,11 @@
+package ru.video01.dmitria.project01.chp04.lesson_01;
+
+public class Out
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+        System.out.println(1.2);
+
+    }
+}
