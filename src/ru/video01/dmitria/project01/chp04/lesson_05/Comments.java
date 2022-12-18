45e0666976528186e5a52644815c31feffac32bf
Nikolas Pavlov <devnikolaspavl@gmail.com>
2022-12-18 14:15:28 +0300

    video01. project01. chp04. lesson_05

---
+++ src/ru/video01/dmitria/project01/chp04/lesson_05/Comments.java
@@ -0,0 +1,18 @@
+package ru.video01.dmitria.project01.chp04.lesson_05;
+
+public class Comments
+{
+    public static void main(String[] args)
+    {
+        // комментарий
+
+        /*
+        Многострочный комментарий
+         */
+
+        /**
+         * doc - комментарий
+         */
+
+    }
+}
