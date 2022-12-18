7709bc8cb1c524f018c644c4c60d7d6dcd42627c
Nikolas Pavlov <devnikolaspavl@gmail.com>
2022-12-18 14:12:29 +0300

    video01. project01. chp04. lesson_04

---
+++ src/ru/video01/dmitria/project01/chp04/lesson_04/VarDefinition.java
@@ -0,0 +1,11 @@
+package ru.video01.dmitria.project01.chp04.lesson_04;
+
+public class VarDefinition
+{
+    public static void main(String[] args)
+    {
+        var i =1;
+        System.out.println(i);
+
+    }
+}
